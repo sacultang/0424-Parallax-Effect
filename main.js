08aa6ed57d86a820f41d669ecbdb800a8ae0d225
sacultang <sacultang@gmail.com>
2022-04-25 20:33:40 +0900

    add : js

---
+++ main.js
@@ -0,0 +1,22 @@
+const scrollReveal = function () {
+	const reveal = document.querySelectorAll('.reveal')
+
+	let scrollTop = window.scrollY || document.documentElement.scrollY
+	// console.log(scrollTop)
+	reveal.forEach((element) => {
+		const revealDelay = element.dataset.delay
+		// if (scrollTop > element.parentElement.offsetTop - 500) {
+		// 	element.classList.add('show')
+		// }
+		if (scrollTop > element.parentElement.offsetTop - 500) {
+			if (revealDelay === undefined) {
+				element.classList.add('show')
+			} else {
+				setTimeout(() => {
+					element.classList.add('show')
+				}, 500)
+			}
+		}
+	})
+}
+window.addEventListener('scroll', scrollReveal)
